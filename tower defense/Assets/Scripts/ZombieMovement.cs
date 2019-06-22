4ef2d3ed574c8d68aa52157da11e56b60ec088f5
leomani3 <leoveyssiere.pro@hotmail.fr>
2019-06-22 14:05:19 +0200

    Zombie : movement

---
+++ tower defense/Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        target = WaypointManager.waypoints[0];
+    }
+
+    private void FixedUpdate()
+    {
+        //Si le zombie est arrivé au waypoint courant, il prend le suivant
+        if(Vector3.Distance(transform.position, target.position) < 0.1)
+        {
+            getNextWaypoint();
+        }
+
+        //le zombie regarde en direction du waypint en tout temps et avance tout droit
+        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+        rb.MovePosition(transform.position + (transform.forward * speed * Time.fixedDeltaTime));
+    }
+
+    private void getNextWaypoint()
+    {
+        if(waypointIndex < WaypointManager.waypoints.Length - 1)
+        {
+            waypointIndex++;
+            target = WaypointManager.waypoints[waypointIndex];
+        }
+    }
+}
