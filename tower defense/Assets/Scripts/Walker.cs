dc77fa568d360f16072cfc446ad46b772c36d612
leomani3 <leoveyssiere.pro@hotmail.fr>
2019-06-22 22:07:42 +0200

    Des changements sur les zombies (faut finir l'attaque) début d'ajout de la base

---
+++ tower defense/Assets/Scripts/Walker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Walker : Zombie
+{
+    private void Start()
+    {
+        Setup();
+        timeBetweenAttacks = 1;
+    }
+
+    private void Update()
+    {
+        CheckIfDie();
+
+        timeCount -= Time.deltaTime;
+
+        if (target != null)
+        {
+            navMeshAgent.SetDestination(target.transform.position);
+            if (CheckDistanceAttack() && timeCount <= 0)
+            {
+                Debug.Log("ATTACK");
+                target.GetComponent<Unit>().health--;
+                timeCount = timeBetweenAttacks;
+            }
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+}
