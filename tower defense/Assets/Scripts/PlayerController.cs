d0387cbba7f9f0842c61cd3247742e0271403de6
Homère <homerebourgeois@hotmail.fr>
2019-06-24 01:22:22 +0200

    modif character controller et plus

--- tower defense/Assets/Scripts/PlayerController.cs
+++ tower defense/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
-
+    public int playerNumber;
+    public string characterInputString;
 
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        characterInputString = "character " + playerNumber + " ";
     }
 
     // Update is called once per frame
@@ -28,14 +30,28 @@ public class PlayerController : MonoBehaviour
 
 
         //MANETTE : LE PLAYER REGARDE EN DIRECTION DE LA SOURIS
-        //---------------------------------------------------
 
-        float hAxis = Input.GetAxisRaw("Horizontal");
-        float vAxis = Input.GetAxisRaw("Vertical");
+        float horizontalAim = Input.GetAxisRaw(characterInputString + "aim horizontal");
+        float verticalAim = Input.GetAxisRaw(characterInputString + "aim vertical");
+
+        transform.LookAt(new Vector3(transform.position.x+verticalAim, transform.position.y, transform.position.z+horizontalAim));
+
+        float hAxis = Input.GetAxisRaw(characterInputString + "move horizontal");
+        float vAxis = Input.GetAxisRaw(characterInputString + "move vertical");
 
         Vector3 moveRight = Vector3.forward * hAxis * speed * Time.deltaTime;
-        Vector3 moveUp = -Vector3.right * vAxis * speed * Time.deltaTime;
+        Vector3 moveUp = Vector3.right * vAxis * speed * Time.deltaTime;
 
         rb.MovePosition(transform.position + moveRight + moveUp);
+
+        //---------------------------------------------------
+
+        //float hoAxis = Input.GetAxisRaw("Horizontal");
+        //float veAxis = Input.GetAxisRaw("Vertical");
+
+        //moveRight = Vector3.forward * hoAxis * speed * Time.deltaTime;
+        //moveUp = -Vector3.right * veAxis * speed * Time.deltaTime;
+
+        //rb.MovePosition(transform.position + moveRight + moveUp);
     }
 }
