1517835b8ee248e5c782ce9813f570b7f9e80229
leomani3 <leoveyssiere.pro@hotmail.fr>
2019-06-24 02:14:04 +0200

    Player : début d'implémentation des modes d'actions

--- tower defense/Assets/Scripts/Player.cs
+++ tower defense/Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class Player : Unit
 {
+    public Image modeImage;
+    
+    private int mode = 1; //1: action 2: construction
+
     // Start is called before the first frame update
     void Start()
     {
+        modeImage.color = new Color(1, 0, 0, 1);
     }
 
     // Update is called once per frame
@@ -14,9 +18,30 @@ public class Player : Unit
     {
         base.Update();
 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeMode();
+        }
+
         if (Input.GetKeyDown(KeyCode.Delete))
         {
             TakeDamage(1);
+        }
+
+
+    }
+
+    public void ChangeMode()
+    {
+        if(mode == 1)
+        {
+            mode = 2;
+            modeImage.color = new Color(0, 1, 1, 1);
+        }
+        else if (mode == 2)
+        {
+            mode = 1;
+            modeImage.color = new Color(1, 0, 0, 1);
         }
     }
 }
